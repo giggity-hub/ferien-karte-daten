231406762e296331ce88ed0bcbc916cc81eb66c3
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-09-28 08:51:58 +0200

    added gesetzliche feiertage

--- script.py
+++ script.py
@@ -6,10 +6,15 @@ from datetime import date
 from typing import List, Dict, TypedDict
 import json
 
+ICAL_DATA_FOLDER = './data/schulferien'
+BUNDESLAENDER = ['BW', 'BY', "BE", "BB", 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
+
 class Holiday(TypedDict):
     holiday_type: str
     start: date
     end: date
+
+HolidayDict = Dict[str, Dict[str, List[Holiday]]]
 
 def icalendar_event_to_holiday(e: icalendar.Event):
     return {
@@ -17,7 +22,7 @@ def icalendar_event_to_holiday(e: icalendar.Event):
         'start': e['DTSTART'].dt,
         'end': e['DTEND'].dt}
 
-def getmopedsschmopeds(file_path) -> List[Holiday]:
+def get_holidays_from_ics_file(file_path) -> List[Holiday]:
     holidays = []
     with open(file_path) as f:
         ical_data = f.read()
@@ -28,31 +33,36 @@ def getmopedsschmopeds(file_path) -> List[Holiday]:
     return holidays
 
 
-ical_data_folder = './ics'
-# years = [dir for dir in os.listdir(ical_data_folder)]
-years = sorted(['2023', '2024'])
-data: Dict[str, Dict[str, List[Holiday]]] = {}
+def duplicate_overflowing_holidays_into_next_year(data: HolidayDict):
+    # All Holidays are only listed once in the year in which they start.
+    # This function duplicates Holidays which go from december to january of the next year into the next year
+    years = list(data.keys())
+    for year in reversed(years[:-1]):
+        last_date_of_year = date(int(year), 12, 31)
+        next_year = str(int(year) + 1)
+        for bundesland in BUNDESLAENDER:
+            for holiday in data[year][bundesland]:
+                if holiday['end'] > last_date_of_year:
+                    data[next_year][bundesland].append(holiday)
+    return data
 
-bundeslaender = ['BW', 'BY', "BE", "BB", 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
+def main():
+    years = sorted([dir for dir in os.listdir(ICAL_DATA_FOLDER)])
+    
+    data: HolidayDict = {}
+    for year in years:
+        data[year] = {}
+        for bundesland in BUNDESLAENDER:
+            file_path = os.path.join(ICAL_DATA_FOLDER, year, f'{bundesland}.ics')
+            data[year][bundesland] = get_holidays_from_ics_file(file_path)
 
-print(years)
-for year in years:
-    data[year] = {}
-    for bundesland in bundeslaender:
-        file_path = os.path.join(ical_data_folder, year, f'{bundesland}.ics')
-        data[year][bundesland] = getmopedsschmopeds(file_path)
+    data = duplicate_overflowing_holidays_into_next_year(data)
 
-
-
-for year in reversed(years[:-1]):
-    last_date_of_year = date(int(year), 12, 31)
-    next_year = str(int(year) + 1)
-    for bundesland in bundeslaender:
-        for holiday in data[year][bundesland]:
-            if holiday['end'] > last_date_of_year:
-                data[next_year][bundesland].append(holiday)
+    with open('data.json', 'w') as f:
+        # Default str necessary for datetime.date
+        json.dump(data, f, default=str, indent=4)
 
 
-with open('data.json', 'w') as f:
-    # Default str necessary for datetime.date
-    json.dump(data, f, default=str, indent=4)
+
+if __name__ == "__main__":
+    main()
